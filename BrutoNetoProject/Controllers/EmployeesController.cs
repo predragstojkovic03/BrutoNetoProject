fa524c8e0da48ac2da87f17d428c1e7bab4339a6
predragstojkovic03 <pedja13331@gmail.com>
2022-02-10 13:40:59 +0100

    ssd backup

--- BrutoNetoProject/Controllers/EmployeesController.cs
+++ BrutoNetoProject/Controllers/EmployeesController.cs
@@ -17,7 +17,7 @@ namespace BrutoNetoProject.Controllers
 
         public EmployeesController(IConfiguration configuration)
         {
-            string connectionString = configuration["ConnectionStrings:Firma"];
+            string connectionString = configuration["ConnectionStrings:Kuca"];
             _employeeDal = new EmployeeDal(connectionString);
         }
 
--- BrutoNetoProject/Models/AssociatedCostViewModel.cs
+++ BrutoNetoProject/Models/AssociatedCostViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,22 @@ namespace BrutoNetoProject.Models
 {
     public class AssociatedCostViewModel
     {
+        [DisplayName("Porez")]
         public decimal TaxRate { get; set; }
+
+        [DisplayName("PIO na teret zaposlenog")]
         public decimal InsuranceOnEmployee { get; set; }
+
+        [DisplayName("Zdravstvo na teret zaposlenog")]
         public decimal HealthCareOnEmployee { get; set; }
+
+        [DisplayName("Nezaposlenost na teret zaposlenog")]
         public decimal UnemploymentOnEmployee { get; set; }
+
+        [DisplayName("PIO na teret poslodavca")]
         public decimal InsuranceOnEmployer { get; set; }
+
+        [DisplayName("Zdravstvo na teret poslodavca")]
         public decimal HealthCareOnEmployer { get; set; }
 
         public static AssociatedCostViewModel MapFrom(AssociatedCostDto associatedCostDto)
